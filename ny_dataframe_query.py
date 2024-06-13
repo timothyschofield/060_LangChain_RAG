61b9615af06bf49c7c6dd0ac3a288f5aef1f6930
Tim Schofield <timothyschofield@hotmail.com>
2024-06-13 12:23:27 +0100

    Matching Authority file

--- ny_dataframe_query.py
+++ ny_dataframe_query.py
@@ -5,19 +5,7 @@
     Author: Tim Schofield
     Date: 12 June 2024
 
-    pd_df.query('column_name.str.contains("abc")', engine='python')
-
-    A trick I just came up with for "starts with":
-    pandas.query('"abc" <= column_name <= "abc~"')
-
-    df.query('column_a.str.contains("abc") or column_b.str.contains("xyz") and column_c>100', engine='python')
-
-
-    Query uses the pandas eval() and is limited in what you can use within it. 
-    If you want to use pure SQL you could consider pandasql where the following statement would work for you:
-    sqldf("select col_name from df where col_name like 'abc%';", locals())
-
-    Pandas Query Examples: SQL-like queries in dataframes
+    See test_dataframe_query.py for examples
     https://queirozf.com/entries/pandas-query-examples-sql-like-syntax-queries-in-dataframes
 
 
@@ -31,16 +19,17 @@ from openai import OpenAI
 
 MODEL = "gpt-4o" # Context window of 128k max_tokens 4096
 
-project_name = "ny_geo_authority_dataframe"
+project_name = "ny_geo_authority_DATAFRAME"
 
 # The file to be compared against the authority database
 input_folder = "ny_hebarium_location_csv_input"
 
-input_transcibed_file = "NY_specimens_transcribed.csv" # ###### Note: this is the one that they gave us ######
-input_transcibed_path = Path(f"{input_folder}/{input_transcibed_file}")
 
 input_authority_file = "NY_Geopolitical_Lookup_Lists_50000.csv"
 input_authority_path = Path(f"{input_folder}/{input_authority_file}")
+
+input_transcibed_file = "NY_specimens_transcribed.csv" # ###### Note: this is the one that they gave us ######
+input_transcibed_path = Path(f"{input_folder}/{input_transcibed_file}")
 
 output_folder = "ny_hebarium_location_csv_output"
 
@@ -60,17 +49,39 @@ except Exception as ex:
 df_authority = pd.read_csv(input_authority_path)
 #print(df_authority.head()
 
+# irn  DarGlobalUniqueIdentifier DarInstitutionCode  DarCatalogNumber  DarRelatedInformation ... SpeOtherSpecimenNumbers_tab   
+df_transcribed = pd.read_csv(input_transcibed_path)
+# print(df_transcribed.head())
 
+output_list = []
+count = 0
+print("####################################### START OUTPUT ######################################")
+for index, row in df_transcribed.iterrows():
+    
+    #row = df.iloc[0]
+    
+    count+=1
+    print(f"{count=}")
+    
+    irn = row["irn"]  
+    continent = row["DarContinent"]
+    country = row["DarCountry"]
+    state_province =  row["DarStateProvince"]
+    county = row["DarCounty"]
 
+    # Get a line from Transcribed and get match(s) in Authority
 
+    # Fake line from Transcribed
+    irn = "999999"
+    continent = "Africa"
+    country = "Ethiopia"
+    state_province =  "Oromia"
+    county = "Mirab Wellega (Zone)"
 
-
-
-
-
-
-
-
+    # Search Authority for match(s) with Transcribed line
+    authority_matches = df_authority.query("(Continent == @continent) and (Country == @country) and (stateProvince == @state_province) and (County == @county)")
+    print(authority_matches)
+    exit()
 
 
 
